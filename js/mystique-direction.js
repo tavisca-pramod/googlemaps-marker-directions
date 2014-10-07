f202f3900cc98c6ae333167133fbc24fb24db6c9
pramod <pramod@tavisca.com>
2014-10-07 10:30:38 +0530

    done with first sub module of step drections

--- js/mystique-direction.js
+++ js/mystique-direction.js
@@ -13,15 +13,19 @@ App.ApplicationController = Ember.Controller.extend({
 App.DirectionsView = Ember.View.extend({
   templateName: 'map-direction',
   
-  name : 'Google Map Directions',
+  name: 'Google Map Directions',
 
-  map : null,
+  map: null,
 
-  startLocation :null,
+  startLocation: null,
 
-  endLocation :null,  
+  endLocation: null, 
 
-   locations: [
+  stepDisplay: null,
+  
+  stepMarkers: null,
+
+  locations: [
     {
        id:1,
        name: 'chicago, il',
@@ -49,7 +53,7 @@ App.DirectionsView = Ember.View.extend({
   }.observes('endLocation'),
 
   mapObservable: function() {
-  // directions map assignment
+    directionsDisplay.setMap(map);
   }.observes('map'),
 
   directionsService: null,
@@ -57,7 +61,7 @@ App.DirectionsView = Ember.View.extend({
   directionsDisplay: null,
 
   initDirection: function() { 
-   
+    stepMarkers= [];
     directionsService = new google.maps.DirectionsService();
     directionsDisplay = new google.maps.DirectionsRenderer();
     
@@ -65,16 +69,55 @@ App.DirectionsView = Ember.View.extend({
       zoom:7,
       center: new google.maps.LatLng(41.850033, -87.6500523)
     };
+
     map = new google.maps.Map($('#map-canvas')[0], mapOptions);
 
     directionsDisplay.setMap(map);
+
+    stepDisplay = new google.maps.InfoWindow();
  
   }.on('didInsertElement'),
-
+  
   calcDirection : function() { 
   
+    function setMarkerContent(marker, text) {
+       google.maps.event.addListener(marker, 'click', function() {
+        // Open an info window when the marker is clicked on,
+        // containing the text of the step location.
+        stepDisplay.setContent(text);
+        
+        stepDisplay.open(map, marker);
+      });
+    };
+
+    function  markLocations(directionResult) {
+        // For each step, placing a marker and adding the data to the marker's
+        // info window.
+        // Attach the marker to an array to keep track of it 
+        var selectedRoute = directionResult.routes[0].legs[0];
+
+        for (var i = 0; i < selectedRoute.steps.length; i++) {
+    
+        var marker = new google.maps.Marker({
+          position: selectedRoute.steps[i].start_location,
+          map: map
+        });
+
+        setMarkerContent(marker, selectedRoute.steps[i].instructions);
+        stepMarkers[i] = marker;
+      }
+    };
+
+
     if(this.get('startLocation') && this.get('endLocation'))
-    {
+    {    
+      // First, remove any existing markers from the map and clear the array.
+      for (var i = 0; i < stepMarkers.length; i++) {
+       stepMarkers[i].setMap(null);
+      }
+
+      stepMarkers = [];
+      
       var request = {
         origin:this.get('startLocation').name,
         destination: this.get('endLocation').name,
@@ -84,11 +127,13 @@ App.DirectionsView = Ember.View.extend({
       directionsService.route(request, function(response, status) {
         if (status == google.maps.DirectionsStatus.OK) {
           directionsDisplay.setDirections(response);
+          markLocations(response);
         }
       });
     }
-  }
+  },
   //end of calcDirection
+  
 });
 
 
