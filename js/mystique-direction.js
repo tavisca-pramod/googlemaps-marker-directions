6bd91dba98087e2a2204d3bdaa504bb89ca0e8ad
pramod <pramod@tavisca.com>
2014-10-07 09:10:27 +0530

    basic direction implemented

--- js/mystique-direction.js
+++ js/mystique-direction.js
@@ -1,31 +1,94 @@
-var directionsDisplay;
-var directionsService = new google.maps.DirectionsService();
-var map;
+window.App = Ember.Application.create();
 
-function initialize() {
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
-  var mapOptions = {
-    zoom:7,
-    center: chicago
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  directionsDisplay.setMap(map);
-}
+App.ApplicationRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    controller.set('title', "Directions service!");
+  }
+});
 
-function calcRoute() {
-  var start = document.getElementById('start').value;
-  var end = document.getElementById('end').value;
-  var request = {
-      origin:start,
-      destination:end,
-      travelMode: google.maps.TravelMode.DRIVING
-  };
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
+App.ApplicationController = Ember.Controller.extend({
+  appName: 'Google Map Directions'
+});
+
+App.DirectionsView = Ember.View.extend({
+  templateName: 'map-direction',
+  
+  name : 'Google Map Directions',
+
+  map : null,
+
+  startLocation :null,
+
+  endLocation :null,  
+
+   locations: [
+    {
+       id:1,
+       name: 'chicago, il',
+       displayName: 'Chicago'
+    },
+
+    {
+      id: 2,
+      name:'st louis, mo',
+      displayName: 'St Louis'
+    },
+
+    {
+      id: 3,
+      name:'joplin, mo',
+      displayName: 'Joplin, MO'
+    }],
+
+  startLocationObservable: function() {
+    this.calcDirection();
+  }.observes('startLocation'),
+
+  endLocationObservable: function() {
+    this.calcDirection();
+  }.observes('endLocation'),
+
+  mapObservable: function() {
+  // directions map assignment
+  }.observes('map'),
+
+  directionsService: null,
+  
+  directionsDisplay: null,
+
+  initDirection: function() { 
+   
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+    var mapOptions = {
+      zoom:7,
+      center: chicago
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
+ 
+  }.on('didInsertElement'),
+
+  calcDirection : function() { 
+  
+    if(this.get('startLocation') && this.get('endLocation'))
+    {
+      var request = {
+        origin:this.get('startLocation').name,
+        destination: this.get('endLocation').name,
+        travelMode: google.maps.TravelMode.DRIVING
+      };  
+
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        }
+      });
     }
-  });
-}
+  }
+  //end of calcDirection
+});
 
-google.maps.event.addDomListener(window, 'load', initialize);
+
+
